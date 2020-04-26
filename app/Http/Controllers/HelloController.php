2b982a68b725ea6790510cf422b68cd4785040ac
ryo <ryo@mba.local>
2020-04-26 20:13:09 +0900

    this is my first commit

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    public function about()
+    {    
+        return view('about');
+    }
+
+    public function services()
+    {    
+        $services = [
+            'Service 1',
+            // 'Service 2',
+            // 'Service 3',
+            // 'Service 4',
+        ];
+
+        return view('services', compact('services'));
+    }
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('app')
+
+@section('title', 'About Us Page')
+
+@section('content')
+    <h1>Welcome to Laravel 6 from about</h1>
+    <p>With some text.</p>
+@endsection
